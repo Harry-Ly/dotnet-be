ff4e7a94f6b4a6ec051d8afbe0d20ed532b50aea
Harry <harry.ly@ibm.com>
2023-01-31 13:53:53 -0600

    Added sql file to remember queries

---
+++ API/sampleData.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Users" ("Id", "UserName")
+VALUES (1, 'Bob');
+
+INSERT INTO "Users" ("Id", "UserName")
+VALUES (2, 'Bobby');
+
+INSERT INTO "Users" ("Id", "UserName")
+VALUES (3, 'Baba');
