b5653ea4b693b0bcfef7bb2f534c5fedc2736c99
Harry <harry.ly@ibm.com>
2023-02-01 17:38:02 -0600

    Added CORS Policy

--- API/Program.cs
+++ API/Program.cs
@@ -10,15 +10,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 }); // This service connects to DB using connection string supplied in the config
 
+builder.Services.AddCors(); // Adds CORS Policy
+
 var app = builder.Build();
 
 // Middleware
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
-
 // app.UseAuthorization(); Temporarily remove until we get to Authorization section
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
