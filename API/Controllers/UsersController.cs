3b4cd7ce6b5c8971cb6a6511babc6a63f6c6e0c4
Harry <harry.ly@ibm.com>
2023-02-18 09:52:02 -0600

    Persisted update changes in the BE

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -33,5 +34,22 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
        return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut] // Updating so use put
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto) // No need to specify the return when updating since it's client side
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // We get username from the auth token that gets sent in every request
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto, user); // EF updates and overwrites User db as it changes, but doesn't save yet
+
+        if (await _userRepository.SaveAllAsync()) return NoContent(); // Saves changes to db and returns 204, meaning Ok but no response to send back
+
+        return BadRequest("Failed to update user");
+
+
     }
 }
