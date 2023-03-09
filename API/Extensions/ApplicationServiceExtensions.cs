180bf367223418c13310ee3c13fe4da9f4837fce
Harry <harry.ly@ibm.com>
2023-03-09 17:12:53 -0600

    Added FlyIO and Github Action

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -11,10 +11,10 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
-        services.AddDbContext<DataContext>(opt =>
-        {
-            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
-        }); // This service connects to DB using connection string supplied in the config
+        // services.AddDbContext<DataContext>(opt =>
+        // {
+        //     opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        // }); // This service connects to DB using connection string supplied in the config
 
         services.AddCors(); // Adds CORS Policy
         services.AddScoped<ITokenService, TokenService>(); // Adds Token Service
--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,36 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityService(builder.Configuration);
 
+// Snippet
+var connString = "";
+if (builder.Environment.IsDevelopment()) 
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else 
+{
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+
+    // Check potential spacing if errors occur
+    connString = $"Server={pgHost}; Port={pgPort}; User Id={pgUser}; Password={pgPass}; Database={pgDb};";
+    //"Server=host.docker.internal; Port=5432; User Id=postgres; Password=postgrespw; Database=datingapp"
+}
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+}); // This service connects to DB using connection string supplied in the config
+// End snippet
+
 var app = builder.Build();
 
 // Middleware
@@ -24,15 +54,19 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder
     .AllowAnyHeader()
     .AllowAnyMethod()
-    .AllowCredentials()
+    .AllowCredentials() 
     .WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseDefaultFiles(); // Gets index.html from wwwroot 
+app.UseStaticFiles(); // Looks for wwwroot folder and serves content from there
     
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 // Database migrations in code
 using var scope = app.Services.CreateScope();
@@ -43,7 +77,8 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [Connections]"); // Bigger scale databases
+    await Seed.ClearConnections(context);
+    // await context.Database.ExecuteSqlRawAsync("TRUNCATE TABLE \"Connections\""); // Bigger scale databases
     // context.Connections.RemoveRange(context.Connections); // Good for small scale remove operations
     await Seed.SeedUsers(userManager, roleManager);
 }
